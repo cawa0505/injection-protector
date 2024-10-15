177f698999bf9e61e5ae887175f0fb8dfec1ad21
aswin8959w <99708785+aswin8959w@users.noreply.github.com>
2024-10-15 12:31:50 +0530

    artisan command added for scan

--- src/SqlQueryProtectionServiceProvider.php
+++ src/SqlQueryProtectionServiceProvider.php
@@ -13,7 +13,6 @@ class SqlQueryProtectionServiceProvider extends ServiceProvider
         $this->commands([
             SqlProtectionCommand::class
         ]);
-
         // Register services, merge configurations
         $this->mergeConfigFrom(__DIR__.'/../config/sqlqueryprotection.php', 'sqlqueryprotection');
     }
